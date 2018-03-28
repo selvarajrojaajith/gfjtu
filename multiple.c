59de63f4a7d55ccdbdf70240a4a79eb7a502c184
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-03-28 16:31:50 +0530

    Create multiple.c

---
+++ multiple.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int  countmultiplier(M)
+{
+    int res = 0;
+    for (int i=1; i<=n; i++)
+    
+           res++;
+ 
+    return res;
+}
+ 
+int main()
+{
+   cout << "Count = " << countMultiples(25);
+return 0;
+}
